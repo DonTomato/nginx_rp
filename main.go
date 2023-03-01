7e2fabe42be5daa6e0b2d1f54b45d0616af59cbb
Mikhail Voronov <michael.voronov@gmail.com>
2023-03-01 21:39:01 +0100

    minor changes

--- main.go
+++ main.go
@@ -8,16 +8,18 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const version = 3
+
 func main() {
 	serviceId := os.Getenv("ID")
 	port := os.Getenv("PORT")
 
-	fmt.Printf("API Service #%s\n", serviceId)
+	fmt.Printf("API Service v%d #%s\n", version, serviceId)
 
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"ping": fmt.Sprintf("OK #%s", serviceId),
+			"ping": fmt.Sprintf("OK v%d: #%s", version, serviceId),
 		})
 	})
 	r.Run(fmt.Sprintf("0.0.0.0:%s", port))
